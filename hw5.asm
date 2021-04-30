d84ce9332e053ff0fb73f79d8087d8523e3d8c1e
Kevin Li <kevinli4321@gmail.com>
2021-04-30 08:45:03 -0400

    Part 2 done

--- hw5.asm
+++ hw5.asm
@@ -4,16 +4,18 @@
 .text:
 
 create_term:
+	move $t0, $a0		# Store $a0 as I need it for sbrk
+
 	# Coefficient of 0 or negative exponent
 	li $v0, -1
-	beq $a0, $0, return_create_term
+	beq $t0, $0, return_create_term
 	blt $a1, $0, return_create_term
 	
 	# Allocate memory + fill coefficient/exponent
 	li $a0, 12
 	li $v0, 9
 	syscall
-	sw $a0, 0($v0)
+	sw $t0, 0($v0)
 	sw $a1, 4($v0)
 	sw $0, 8($v0)
 
@@ -21,9 +23,31 @@ create_term:
 	jr $ra
 	
 init_polynomial:
+	addi $sp, $sp, -8
+	sw $ra, 0($sp)
+	sw $s0, 4($sp)		# Store polynomial pointer
+	move $s0, $a0
+
+	lw $a0, 0($a1)		# Coefficient
+	lw $a1, 4($a1)		# Exponent
+	# Coefficient of 0 or negative exponent
+	li $v0, -1
+	beq $a0, $0, return_init_polynomial
+	blt $a1, $0, return_init_polynomial
+	
+	jal create_term		# Call create_term with $a0 and $a1 containing the correct args already
+	sw $v0, 0($s0)		# Store address of polynomial in p -> head_term
+	li $v0, 1
+
+	return_init_polynomial:
+	lw $ra, 0($sp)
+	lw $s0, 4($sp)
+	addi $sp, $sp, 8
 	jr $ra
 	
 add_N_terms_to_polynomial:
+	
+
 	jr $ra
 	
 update_N_terms_in_polynomial:
--- init_polynomial_test.asm
+++ init_polynomial_test.asm
@@ -1,16 +1,39 @@
 .data
-pair: .word 12 8
+pair: .word 2 3
 p: .word 0
 
 .text:
 main:
-    la $a0, p
-    la $a1, pair
-    jal init_polynomial
+	la $a0, p
+   	la $a1, pair
+   	jal init_polynomial
+   	move $s0, $v0
 
-    #write test code
+    	#write test code
+	move $a0, $s0	
+	li $v0, 1
+	syscall
+	li $a0, '\n'
+	li $v0, 11
+	syscall	
+	
+	blt $s0, $0, skip_init_polynomial_test
+	
+	la $t0, p
+	lw $t0, 0($t0)
+	lw $a0, 0($t0)		# Coefficient
+	li $v0, 1
+	syscall
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+	
+	lw $a0, 4($t0)		# Exponent
+	li $v0, 1
+	syscall
 
-    li $v0, 10
-    syscall
+	skip_init_polynomial_test:
+    	li $v0, 10
+    	syscall
 
 .include "hw5.asm"
